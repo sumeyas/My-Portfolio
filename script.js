0f2cd3c1ffc52105a0fceedf6e7e413f34bf7ded
sumeya seid <sumeyaskr@gmail.com>
2022-04-17 14:26:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const navTogglerBtn=document.querySelector(".nav-toggler"),
+aside=document.querySelector(".aside");
+navTogglerBtn.addEventListener("click",()=>
+{
+    asideSectionTogglerBtn();
+})
+function asideSectionTogglerBtn()
+{
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open")
+}
+
+
